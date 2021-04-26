39f214c27cdd9c2351564dc5b3ee35da280c36b4
dm-vev <80632449+dm-vev@users.noreply.github.com>
2021-04-26 17:40:35 +0300

    Update TemplateExample.py

--- Examples/TemplateExample.py
+++ Examples/TemplateExample.py
@@ -1,15 +1,18 @@
 from libDataManager import *
 
 dm = DataManager()
-tm = Template()
+tm = Template() 
 
 TestTemplate = {
 				'API': 3.17,
 				'Value1': 9,
 				'Value2': 8
-				}
-tm.set(TestTemplate, 'TestTemplate')
+				} #Template example
+tm.set(TestTemplate, 'TestTemplate') #Add to templates array new template
 
-dm.new('TestDTFromTemplate', 'TestTemplate')
-dm.write('TestDTFromTemplate', 'Value3', 17)
-print(dm.getCopy('TestDTFromTemplate'))
+dm.new('TestDTFromTemplate', 'TestTemplate') #Creating new dt from TestTemplate template
+dm.write('TestDTFromTemplate', 'Value3', 17) #Write to dt
+print(dm.getCopy('TestDTFromTemplate')) #Print testdt
+
+tm.save('TestTemplate') #Save template
+tm.load('TestTamplate') #Load template
