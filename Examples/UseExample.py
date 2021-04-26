b66fcafd6b4facf336de7ab5b03432a756ea5684
dm-vev <80632449+dm-vev@users.noreply.github.com>
2021-04-26 17:41:28 +0300

    Update UseExample.py

--- Examples/UseExample.py
+++ Examples/UseExample.py
@@ -3,13 +3,13 @@ from DataManager import DataManager
 dm= DataManager()
 
 new = input('Set new DT name: ')
-dm.new(new)
+dm.new(new) #Creating new dt
 
 data = input('Set value for write to WriteValue: ')
-dm.write(new, 'WriteValue', data)
+dm.write(new, 'WriteValue', data) #Write to dt
 
-print(new+' WriteValue = '+dm.read(new, 'WriteValue'))
+print(new+' WriteValue = '+dm.read(new, 'WriteValue')) #Get value from dt
 
 dm.migrate(new,'MigrateExample', createNew=True) #Now your DT = MigrateExample DT
 
-print(dm.getCopy('MigrateExample'))
+print(dm.getCopy('MigrateExample')) #Print dt
