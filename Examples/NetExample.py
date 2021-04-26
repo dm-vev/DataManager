cda249e841528b507dd7f49b7fd047a3a3eeabe8
dm-vev <80632449+dm-vev@users.noreply.github.com>
2021-04-26 17:43:51 +0300

    Update NetExample.py

--- Examples/NetExample.py
+++ Examples/NetExample.py
@@ -1,14 +1,14 @@
 import socket
 import sys
 
-HOST, PORT = "localhost", 9087
+HOST, PORT = "localhost", 9087 #Set DMServer
 
 print('DMNet Console v0.1')
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Create socket connection to DMServer
 while True:
 	data = input("->")
 	print(data)
-	sock.sendto(bytes(data, "utf-32"), (HOST, PORT))
-	received = str(sock.recv(4098), "utf-32")
+	sock.sendto(bytes(data, "utf-32"), (HOST, PORT)) #Send dat, NOTE! DMServer support only UTF-32 encoding!
+	received = str(sock.recv(4098), "utf-32") #Recieve response from DMServer
 	print('[ Server ] :: '+received)
