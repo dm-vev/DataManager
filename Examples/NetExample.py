bee89ff296a701ca0af763a1a11d55a43df8ef61
dm-vev <80632449+dm-vev@users.noreply.github.com>
2021-07-19 14:09:57 +0300

    Update NetExample.py
    Added authentication for example

--- Examples/NetExample.py
+++ Examples/NetExample.py
@@ -6,9 +6,11 @@ HOST, PORT = "localhost", 9087 #Set DMServer
 print('DMNet Console v0.1')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Create socket connection to DMServer
+
+authkey = input("Auth key (Enter if auth disabled) -> ")
+
 while True:
-	data = input("->")
-	print(data)
-	sock.sendto(bytes(data, "utf-32"), (HOST, PORT)) #Send dat, NOTE! DMServer support only UTF-32 encoding!
+	data = input("CMD ->")
+	sock.sendto(bytes(authkey + data, "utf-32"), (HOST, PORT)) #Send dat, NOTE! DMServer support only UTF-32 encoding!
 	received = str(sock.recv(4098), "utf-32") #Recieve response from DMServer
 	print('[ Server ] :: '+received)
